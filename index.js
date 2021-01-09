d1fc1cd5c788a4c20523da108dead4ea22677e5c
Ryan <ryan-walker@live.com>
2021-01-09 12:52:58 -0500

    add console test message

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type' : 'text/plain'
+    });
+
+    res.write('Hello JS World!');
+    res.end();
+
+}).listen(port);
+
+
+console.log('cat shelter test commit 010921');
